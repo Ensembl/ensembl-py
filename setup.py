681df28cc9c176a417c8cc97f047a96d8462635a
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2020-06-19 17:50:00 +0100

    update setup.py + clean-up

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
-"""
-.. See the NOTICE file distributed with this work for additional information
-   regarding copyright ownership.
+# .. See the NOTICE file distributed with this work for additional information
+#    regarding copyright ownership.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+"""Build script for setuptools."""
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
 from pathlib import Path
 
-from setuptools import setup, find_namespace_packages, find_packages
+from setuptools import setup, find_namespace_packages
+
 
 with open(Path(__file__).parent / 'README.md') as f:
     readme = f.read()
-with open(Path(__file__).parent / 'LICENSE') as f:
-    license_ct = f.read()
 with open(Path(__file__).parent / 'VERSION') as f:
     version = f.read()
 
 
 def import_requirements():
-    """Import ``requirements.txt`` file located at the root of the repository."""
     """Import ``requirements.txt`` file located at the root of the repository."""
     with open(Path(__file__).parent / 'requirements.txt') as file:
         return [line.rstrip() for line in file.readlines()]
@@ -36,7 +34,7 @@ def import_requirements():
 setup(
     name='ensembl-py',
     package_dir={"": "src/python"},
-    packages=find_namespace_packages(include=['ensembl.hive.*']),
+    packages=find_namespace_packages(where='src/python'),
     description="Ensembl Python Base library",
     include_package_data=True,
     install_requires=import_requirements(),
@@ -45,7 +43,7 @@ setup(
     author_email='dev@ensembl.org',
     url='https://www.ensembl.org',
     download_url='https://github.com/Ensembl/ensembl-py',
-    license=license_ct,
+    license="Apache License, Version 2.0",
     setup_requires=[
         'pytest-runner',
     ],
