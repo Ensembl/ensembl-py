5090fa35be1934e6ec2795d9e56f1b05a7b8069c
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-11-19 14:41:19 +0000

    Update src/tests/utils/test_loader.py
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- src/tests/utils/test_loader.py
+++ src/tests/utils/test_loader.py
@@ -9,6 +9,14 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+"""Unit testing of :mod:`utils.rloader` module.
+
+Typical usage example::
+
+    $ pytest test_rloader.py
+
+"""
+
 import unittest
 
 from ensembl.utils.rloader import RemoteFileLoader
