99023169226a9af6ee6f80c54cd53184295009a4
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2023-04-26 11:24:05 +0100

    move all project config into pyproject.toml

--- setup.py
+++ setup.py
@@ -11,56 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Build script for setuptools."""
+"""setuptools-based stub for the "editable" installations"""
 
-import sys
+from setuptools import setup
 
-from setuptools import setup, find_namespace_packages
-
-
-with open('README.md') as f:
-    readme = f.read()
-
-
-with open('VERSION') as f:
-    version = f.read()
-
-
-def import_requirements(requirements_path):
-    """Import file located at the root of the repository."""
-    with open(requirements_path) as file:
-        return [line.rstrip() for line in file.readlines()]
-
-
-setup(
-    name='ensembl-py',
-    version=version,
-    packages=find_namespace_packages(where='src/python'),
-    package_dir={"": "src/python"},
-    description="Ensembl Python Base library",
-    include_package_data=True,
-    install_requires=import_requirements('requirements.txt'),
-    tests_require=import_requirements('requirements-test.txt'),
-    long_description=readme,
-    author='Ensembl',
-    author_email='dev@ensembl.org',
-    url='https://www.ensembl.org',
-    download_url='https://github.com/Ensembl/ensembl-py',
-    license="Apache License 2.0",
-    python_requires=">=3.8",
-    entry_points={
-        'pytest11': [
-            'name_of_plugin = ensembl.plugins',
-        ]
-    },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
-)
+if __name__ == "__main__":
+    setup()
