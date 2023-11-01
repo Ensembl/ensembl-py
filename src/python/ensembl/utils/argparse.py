e12ffccb6b6edfde9e80328cb12770a31b0ec63c
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2023-11-01 12:27:33 +0000

    update methods to improve functionality

--- src/python/ensembl/utils/argparse.py
+++ src/python/ensembl/utils/argparse.py
@@ -20,12 +20,12 @@ import argparse
 import os
 from pathlib import Path
 
-import sqlalchemy
+from sqlalchemy.engine import make_url, URL
 
 
 def _validate_src_path(parser: argparse.ArgumentParser, src_path: os.PathLike) -> Path:
     """Returns the path if exists and it is readable, raises an error through the argument parser otherwise.
-    
+
     Args:
         parser: Argument parser.
         src_path: File or directory path to check.
@@ -41,7 +41,7 @@ def _validate_src_path(parser: argparse.ArgumentParser, src_path: os.PathLike) -
 
 def _validate_dst_path(parser: argparse.ArgumentParser, dst_path: os.PathLike, exists_ok: bool) -> Path:
     """Returns the path if it is writable, raises an error through the argument parser otherwise.
-    
+
     Args:
         parser: Argument parser.
         dst_path: File or directory path to check.
@@ -65,7 +65,7 @@ def _validate_dst_path(parser: argparse.ArgumentParser, dst_path: os.PathLike, e
 
 class ArgumentParser(argparse.ArgumentParser):
     """Extends :class:`argparse.ArgumentParser` with additional methods and functionality.
-    
+
     The default behaviour of the help text will be to display the default values on every non-required
     argument, i.e. optional arguments with "required=False".
 
@@ -75,10 +75,11 @@ class ArgumentParser(argparse.ArgumentParser):
         """Extends the base class to include the information about default argument values by default."""
         super().__init__(*args, **kwargs)
         self.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+        self.__server_groups = []
 
     def add_argument(self, *args, **kwargs) -> None:
         """Extends the parent function by excluding the default value in the help text when not provided.
-        
+
         Only applied to required arguments without a default value, i.e. positional arguments or optional
         arguments with "required=True".
 
@@ -98,7 +99,7 @@ class ArgumentParser(argparse.ArgumentParser):
         kwargs.pop("type", None)
         self.add_argument(*args, **kwargs, type=lambda x: _validate_src_path(self, x))
 
-    def add_argument_dst_path(self, exists_ok: bool = True, *args, **kwargs) -> None:
+    def add_argument_dst_path(self, *args, exists_ok: bool = True, **kwargs) -> None:
         """Adds :class:`pathlib.Path` argument, checking if it is writable at parsing time.
 
         If "metavar" is not defined it is added with "PATH" as value to improve help text readability.
@@ -121,32 +122,66 @@ class ArgumentParser(argparse.ArgumentParser):
         if "metavar" not in kwargs:
             kwargs["metavar"] = "URI"
         kwargs.pop("type", None)
-        self.add_argument(*args, **kwargs, type=lambda x: sqlalchemy.engine.make_url(x))
+        self.add_argument(*args, **kwargs, type=lambda x: make_url(x))
 
-    def add_server_arguments(self, prefix: str = "") -> None:
+    def add_server_arguments(self, prefix: str = "", include_database: bool = False, help: str = "") -> None:
         """Adds the usual set of arguments needed to connect to a server.
-        
+
         This means: "--host", "--port", "--user" and "--password" (optional).
 
         Args:
             prefix: Prefix to add the each argument, e.g. if prefix is "src_", the arguments will be
                 "--src_host", etc.
+            include_database: Include "--database" argument.
+            help: Description message to include for this set of arguments.
 
         """
-        self.add_argument(f"--{prefix}host", required=True, help="Name of the host")
-        self.add_argument(f"--{prefix}port", required=True, type=int, help="Port number")
-        self.add_argument(f"--{prefix}user", required=True, help="User name")
-        self.add_argument(f"--{prefix}password", metavar="PWD", default="", help="Host password")
+        group = self.add_argument_group(f"{prefix}server connection arguments", description=help)
+        group.add_argument(
+            f"--{prefix}host", required=True, default=argparse.SUPPRESS, metavar="HOST", help="Host name"
+        )
+        group.add_argument(
+            f"--{prefix}port",
+            required=True,
+            type=int,
+            default=argparse.SUPPRESS,
+            metavar="PORT",
+            help="Port number",
+        )
+        group.add_argument(
+            f"--{prefix}user", required=True, default=argparse.SUPPRESS, metavar="USER", help="User name"
+        )
+        group.add_argument(f"--{prefix}password", metavar="PWD", help="Host password")
+        if include_database:
+            group.add_argument(
+                f"--{prefix}database",
+                required=True,
+                default=argparse.SUPPRESS,
+                metavar="NAME",
+                help="Database name",
+            )
+        self.__server_groups.append(prefix)
 
-    def add_database_arguments(self, prefix: str = "") -> None:
-        """Adds the usual set of arguments needed to connect to a database.
-        
-        This means: "--host", "--port", "--user", "--password" (optional) and "--database".
+    def parse_args(self, *args, **kwargs) -> argparse.Namespace:
+        """Extends the parent function by adding a new URL argument for every server group added.
 
-        Args:
-            prefix: Prefix to add the each argument, e.g. if prefix is "src_", the arguments will be
-                "--src_host", etc.
+        The type of this new argument will be :class:`sqlalchemy.engine.URL`. It also logs all the parsed
+        arguments for debugging purposes.
 
         """
-        self.add_server_arguments(prefix=prefix)
-        self.add_argument(f"--{prefix}database", required=True, metavar="NAME", help="Database name")
+        args = super().parse_args(*args, **kwargs)
+        # Build and add an sqlalchemy.engine.URL object for every server group added
+        for prefix in self.__server_groups:
+            # Raise an error rather than overwriting when the URL argument is already present
+            if f"{prefix}url" in args:
+                self.error(f"argument '{prefix}url' is already present")
+            server_url = URL.create(
+                "mysql",
+                getattr(args, f"{prefix}user"),
+                getattr(args, f"{prefix}password"),
+                getattr(args, f"{prefix}host"),
+                getattr(args, f"{prefix}port"),
+                getattr(args, f"{prefix}database", None),
+            )
+            setattr(args, f"{prefix}url", server_url)
+        return args
